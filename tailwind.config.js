c26a2ba5291feac374993ad27cb7fbe3d17792b8
synqqrawr <174339922+synqqrawr@users.noreply.github.com>
2024-07-25 14:22:26 +0900

    feat: make use of manrope and jetbrainsmono

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {},
+		fontFamily: {
+			sans: ['Manrope', 'sans-serif'],
+			monospace: ['JetBrains Mono', 'monospace']
+		}
 	},
 	plugins: [require('@tailwindcss/typography')],
 	darkMode: ['selector', "[data-mode='dark']"]
