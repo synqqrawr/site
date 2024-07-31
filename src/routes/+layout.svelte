891ae61acd7daa99df589e3ab315e598f1053c36
synqqrawr <174339922+synqqrawr@users.noreply.github.com>
2024-07-31 20:16:05 +0900

    /*: remove unneeded <main> padding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 <ModeWatcher />
 <div class="layout h-full grid grid-rows-[auto_1fr_auto] px-4">
 	<Header />
-	<main class="break-words p-8">
+	<main class="break-words">
 		<slot />
 	</main>
 	<Footer />
