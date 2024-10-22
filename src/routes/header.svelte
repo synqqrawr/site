f949e5a0839287c5f3f4de21ebc526d6fc677146
synqqrawr <174339922+synqqrawr@users.noreply.github.com>
2024-10-22 17:26:10 +0900

    lol headerz
    my bad
    
    lol

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -10,17 +10,11 @@
 		<b>{config.title}.</b>
 	</a>
 
-	<ul class="mx-8 sm:flex hidden sm:gap-8 links" aria-label="List of pages to navigate to.">
+	<ul class="mx-8 flex gap-8 links" aria-label="List of pages to navigate to.">
 		{#each config.links as link}
 			<li><a href={link.href} target={link.target ?? '_self'}>{link.name ?? link.href}</a></li>
 		{/each}
 	</ul>
-
-	<div class="flex">
-		<div class="sm:hidden">
-			<!-- <Links /> -->
-		</div>
-	</div>
 </nav>
 
 <style class="postcss">
