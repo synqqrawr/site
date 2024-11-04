d1617fdf14e5a22c3a2c7a938401dc2d3758b1d5
synqqrawr <174339922+synqqrawr@users.noreply.github.com>
2024-11-04 18:59:20 +0900

    Minify HTML

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,36 @@
+import { minify } from 'html-minifier'
+import { building } from '$app/environment'
+
+const minification_options = {
+	collapseBooleanAttributes: true,
+	collapseWhitespace: true,
+	conservativeCollapse: true,
+	decodeEntities: true,
+	html5: true,
+	ignoreCustomComments: [/^#/],
+	minifyCSS: true,
+	minifyJS: false,
+	removeAttributeQuotes: true,
+	removeComments: false, // some hydration code needs comments, so leave them in
+	removeOptionalTags: true,
+	removeRedundantAttributes: true,
+	removeScriptTypeAttributes: true,
+	removeStyleLinkTypeAttributes: true,
+	sortAttributes: true,
+	sortClassName: true
+}
+
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
+  let page = '';
 	return resolve(event, {
-		preload: ({ type }) => type === 'font' || type === 'js' || type === 'css'
+		preload: ({ type }) => type === 'font' || type === 'js' || type === 'css',
+
+		transformPageChunk: ({ html, done }) => {
+			page += html;
+			if (done) {
+				return building ? minify(page, minification_options) : page;
+			}
+		}
 	})
 }
