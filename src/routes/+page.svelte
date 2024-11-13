20832dedf633eabfd3235507ba27b6058d559168
synqqrawr <174339922+synqqrawr@users.noreply.github.com>
2024-11-13 22:29:56 +0900

    Remove unneeded nesting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,55 +8,53 @@
 	<meta name="description" content={config.description} />
 </svelte:head>
 
-<article>
+<article class="prose">
 	<!-- Post -->
-	<div class="prose">
-		<h2>Welcome Aboard! 🚀</h2>
-		<p>
-			Hey there, and welcome to my little corner of the internet! Feel free to take a look around.
-			Here are some links to the pages of this site:
-		</p>
-		<ul>
-			{#each config.links as link}
-				<li><a href={link.href} target={link.target ?? '_self'}>{link.label ?? link.href}</a></li>
-			{/each}
-		</ul>
-		<h2>~> /bin/whoami</h2>
-		I'm&nbsp<b>[anon]</b> from Japan. I go by a few different aliases, including:
-		<ul>
-			<li>async</li>
-			<li>asyncedd</li>
-			<li>asynqq</li>
-			<li>asynqqboo</li>
-			<li>sink</li>
-			<li>synqqrawr</li>
-		</ul>
-		<h3>Contact me</h3>
-		<ul>
-			<li>
-				<a href="https://reddit.com/u/asynqq">Reddit</a>
-			</li>
-			<li>
-				<a href="https://discord.com/users/797422750321999943">Discord</a>
-			</li>
-			<li>
-				<a href="https://github.com/synqqrawr">Github</a>
-			</li>
-			<li>
-				<a href="https://sh.itjust.works/u/synqqrawr">Lemmy (sh.itjust.works)</a>
-			</li>
-			<li>
-				<a href="https://fosstodon.org/@synqqrawr">Matsodon (fosstodon.org)</a>
-			</li>
-			<li>
-				<a href="https://matrix.to/#/@synqqrawr:matrix.org">Matrix</a>
-			</li>
-			<li>
-				<a href="mailto:contact.carbon479@passfwd.com">contact.carbon479@passfwd.com</a> |&nbsp;<a
-					target="_blank"
-					href="/gpg.txt">./gpg.txt</a
-				>
-			</li>
-		</ul>
-	</div>
+	<h2>Welcome Aboard! 🚀</h2>
+	<p>
+		Hey there, and welcome to my little corner of the internet! Feel free to take a look around.
+		Here are some links to the pages of this site:
+	</p>
+	<ul>
+		{#each config.links as link}
+			<li><a href={link.href} target={link.target ?? '_self'}>{link.label ?? link.href}</a></li>
+		{/each}
+	</ul>
+	<h2>~> /bin/whoami</h2>
+	I'm&nbsp<b>[anon]</b> from Japan. I go by a few different aliases, including:
+	<ul>
+		<li>async</li>
+		<li>asyncedd</li>
+		<li>asynqq</li>
+		<li>asynqqboo</li>
+		<li>sink</li>
+		<li>synqqrawr</li>
+	</ul>
+	<h3>Contact me</h3>
+	<ul>
+		<li>
+			<a href="https://reddit.com/u/asynqq">Reddit</a>
+		</li>
+		<li>
+			<a href="https://discord.com/users/797422750321999943">Discord</a>
+		</li>
+		<li>
+			<a href="https://github.com/synqqrawr">Github</a>
+		</li>
+		<li>
+			<a href="https://sh.itjust.works/u/synqqrawr">Lemmy (sh.itjust.works)</a>
+		</li>
+		<li>
+			<a href="https://fosstodon.org/@synqqrawr">Matsodon (fosstodon.org)</a>
+		</li>
+		<li>
+			<a href="https://matrix.to/#/@synqqrawr:matrix.org">Matrix</a>
+		</li>
+		<li>
+			<a href="mailto:contact.carbon479@passfwd.com">contact.carbon479@passfwd.com</a> |&nbsp;<a
+				target="_blank"
+				href="/gpg.txt">./gpg.txt</a
+			>
+		</li>
+	</ul>
 </article>
