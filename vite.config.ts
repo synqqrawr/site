866e240f79f4e74212238072b9d377d25af71199
synqqrawr <174339922+synqqrawr@users.noreply.github.com>
2024-12-08 13:45:53 +0900

    rm enhacned-img

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
-import { enhancedImages } from '@sveltejs/enhanced-img'
 import lightningcss from 'vite-plugin-lightningcss'
 
 export default defineConfig({
 	plugins: [
-		enhancedImages(),
 		sveltekit(),
 		lightningcss({
 			browserslist: '>= 0.25%'
